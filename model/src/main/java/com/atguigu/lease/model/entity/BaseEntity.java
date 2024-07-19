a2e07b7eaabe173f3d303459c132f9e9a0d1ff78
Henry.Liao <Henry.Liao@skylinetw.com>
2024-07-19 14:50:02 +0800

    feat: 添加自動填充預設值與ignore isDeleted json 屬性

--- model/src/main/java/com/atguigu/lease/model/entity/BaseEntity.java
+++ model/src/main/java/com/atguigu/lease/model/entity/BaseEntity.java
@@ -1,9 +1,7 @@
 package com.atguigu.lease.model.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -18,16 +16,17 @@ public class BaseEntity implements Serializable {
     private Long id;
 
     @Schema(description = "创建时间")
-    @TableField(value = "create_time")
+    @TableField(value = "create_time", fill = FieldFill.INSERT)
     private Date createTime;
 
     @Schema(description = "更新时间")
-    @TableField(value = "update_time")
+    @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
 
     @Schema(description = "逻辑删除")
     @TableLogic
-    @TableField("is_deleted")
+    @JsonIgnore
+    @TableField(value = "is_deleted")
     private Byte isDeleted;
 
 }
